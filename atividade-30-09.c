1cf83586310fe3b0e53abe53cbb65bdf866ea2d5
MELYSSA SOUZA ARAUJO <126412768+Melyssa-Souza-Araujo@users.noreply.github.com>
2024-09-30 09:24:54 -0300

    Update atividade-30-09.c

--- atividade-30-09.c
+++ atividade-30-09.c
@@ -1,3 +1,4 @@
+//questão 1
 #include <stdio.h>
 #include <string.h>
 
@@ -20,6 +21,40 @@ int main() {
     printf("\nRegistro: %d\n", f.registro);
     printf("Salario: %.2f\n", f.salario);
     printf("Nome: %s\n", f.nome);
+
+    return 0;
+}
+
+//questão 2
+
+#include <stdio.h>
+
+struct Carro {
+    char marca[30];
+    int ano_fabricacao;
+    char cor[20];
+    float preco;
+};
+
+int main() {
+    struct Carro c;
+
+    // Atribuindo valores aos campos
+    printf("Digite a marca do carro: ");
+    scanf("%s", c.marca);
+    printf("Digite o ano de fabricação: ");
+    scanf("%d", &c.ano_fabricacao);
+    printf("Digite a cor do carro: ");
+    scanf("%s", c.cor);
+    printf("Digite o preço: ");
+    scanf("%f", &c.preco);
+
+    // Imprimindo relatório
+    printf("\n--- Relatório do Carro ---\n");
+    printf("Marca: %s\n", c.marca);
+    printf("Ano de Fabricação: %d\n", c.ano_fabricacao);
+    printf("Cor: %s\n", c.cor);
+    printf("Preço: R$ %.2f\n", c.preco);
 
     return 0;
 }
